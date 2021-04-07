f8e269bc9c23f412c019644b0ff8152ecc69e9d8
Denis <deni-kov@mail.ru>
2021-04-07 13:04:24 +0300

    finish

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 calendarId = 'deniskin1992@gmail.com'
 SERVICE_ACCOUNT_FILE = 'iconic-heading-309220-5791268ca5a0.json'
 
+time = ''
+
 
 class GoogleCalendar(object):
 
@@ -16,66 +18,39 @@ class GoogleCalendar(object):
         credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
         self.service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
 
-    # создание словаря с информацией о событии
-    def create_event_dict(self):
-        event = {
-            'summary': 'Denis_test',
-            'description': 'some info',
-            'start': {
-                'dateTime': '2021-03-31T20:30:00+03:00',
-            },
-            'end': {
-                'dateTime': '2021-03-31T21:30:00+03:00',
-            }
-        }
-        return event
+    def get_events_list(self):
 
-    # создание события в календаре
-    def create_event(self, event):
-        e = self.service.events().insert(calendarId=calendarId,
-                                         body=event).execute()
-        print('Event created: %s' % (e.get('id')))
-
-    # вывод списка из десяти предстоящих событий
-    def get_events_list(self, date=None):
+        data = {}
+        now1 = ''
+        i = 0
         now = datetime.datetime.utcnow().isoformat() + 'Z'
-        print('Getting the upcoming 10 events')
+
+        for e in now:
+            i = i + 1
+            now1 = now1 + e
+            if (i == 12):
+                break
+            timeMin = now1 + "05:00:00+03:00"
+            timeMax = now1 + "22:00:00+03:00"
+
+        print(now1)
         events_result = self.service.events().list(calendarId=calendarId,
-                                                   timeMin=now,
-                                                   maxResults=10, singleEvents=True,
+                                                   timeMin=timeMin,
+                                                   timeMax=timeMax, singleEvents=True,
                                                    orderBy='startTime').execute()
         events = events_result.get('items', [])
 
-        data = {}
-        lst = []
-
-        # for i in events:
-        #     lst.append(i['summary'])
-        #     data[now] = lst
-        #     print(data)
-
         if not events:
             print('No upcoming events found.')
-
+        i = 1
         for event in events:
-            lst.append(event['summary'])
-            start = event['start'].get('date')
-            data[start] = lst
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            time = start[11] +start[12] + start[13] + start[14] + start[15]
+            data[time] = event['summary']
+
 
         print(data)
 
 
-
-
 calendar = GoogleCalendar()
-print("+ - create event\n? - print event list\n")
-c = input()
-
-
-if c == '+':
-    event = calendar.create_event_dict()
-    calendar.create_event(event)
-elif c == '?':
-    calendar.get_events_list()
-else:
-    pass
+calendar.get_events_list()
